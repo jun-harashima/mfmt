2e7f57917e03aaf7d703a2dcbcc68f691cde0485
jun-harashima <j.harashima@gmail.com>
2024-11-23 22:51:58 +0900

    modify m2j

--- src/mfmt/mecab.py
+++ src/mfmt/mecab.py
@@ -7,15 +7,26 @@ class Mecab:
     def __init__(self) -> None:
         pass
 
-    def to_juman(self, input_lines: list[str]) -> list[str]:
+    def to_juman(self, input_txt: Path) -> None:
+        with open(input_txt) as file:
+            lines: list[str] = []
+            for line in file:
+                line = line.rstrip("\n")
+                if line == "EOS":
+                    output_lines = self._to_juman(lines)
+                    for output_line in output_lines:
+                        print(output_line)
+                    print("EOS")
+                    lines = []
+                else:
+                    lines.append(line)
+
+    def _to_juman(self, lines: list[str]) -> list[str]:
         output_lines = []
-        for input_line in input_lines:
-            if input_line == "EOS":
-                output_lines.append("EOS")
-            else:
-                midashi, hinshi1, hinshi2, yomi = self._split(input_line)
-                output_line = f"{midashi} {yomi} * {hinshi1} * {hinshi2} * * * * * *"
-                output_lines.append(output_line)
+        for line in lines:
+            midashi, hinshi1, hinshi2, yomi = self._split(line)
+            output_line = f"{midashi} {yomi} * {hinshi1} * {hinshi2} * * * * * *"
+            output_lines.append(output_line)
         return output_lines
 
     def to_kytea(self, input_txt: Path) -> None:
@@ -24,8 +35,8 @@ class Mecab:
             for line in file:
                 line = line.rstrip("\n")
                 if line == "EOS":
-                    line = self._to_kytea(lines)
-                    print(line)
+                    output_line = self._to_kytea(lines)
+                    print(output_line)
                     lines = []
                 else:
                     lines.append(line)
@@ -36,8 +47,8 @@ class Mecab:
             midashi, hinshi1, _, yomi = self._split(line)
             word = f"{midashi}/{hinshi1}/{yomi}"
             words.append(word)
-        line = " ".join(words)
-        return line
+        output_line = " ".join(words)
+        return output_line
 
     def _split(self, line: str) -> tuple[str, str, str, str]:
         midashi, rest = line.split("\t")
--- tests/test_mecab.py
+++ tests/test_mecab.py
@@ -1,21 +1,16 @@
+import pytest
+
 from mfmt.mecab import Mecab
 
 
-def test_to_juman() -> None:
-    with open("tests/mecab/input.txt") as file:
-        input_lines = file.read().splitlines()
-
+@pytest.fixture
+def mecab() -> Mecab:
     mecab = Mecab()
-    actual_lines = mecab.to_juman(input_lines)
-
-    with open("tests/mecab/output_juman.txt") as file:
-        expected_lines = file.read().splitlines()
-
-    assert actual_lines == expected_lines
+    return mecab
 
 
-def test__to_kytea() -> None:
-    mecab = Mecab()
+@pytest.fixture
+def lines() -> list[str]:
     lines = [
         "今日	名詞,副詞可能,*,*,*,*,今日,キョウ,キョー",
         "は	助詞,係助詞,*,*,*,*,は,ハ,ワ",
@@ -23,14 +18,28 @@ def test__to_kytea() -> None:
         "です	助動詞,*,*,*,特殊・デス,基本形,です,デス,デス",
         "。	記号,句点,*,*,*,*,。,。,。",
     ]
+    return lines
+
+
+def test__to_juman(mecab, lines) -> None:
+    actual = mecab._to_juman(lines)
+    expected = [
+        "今日 きょう * 名詞 * 副詞可能 * * * * * *",
+        "は は * 助詞 * 係助詞 * * * * * *",
+        "雨 あめ * 名詞 * 一般 * * * * * *",
+        "です です * 助動詞 * * * * * * * *",
+        "。 。 * 記号 * 句点 * * * * * *",
+    ]
+    assert actual == expected
+
+
+def test__to_kytea(mecab, lines) -> None:
     actual = mecab._to_kytea(lines)
     expected = "今日/名詞/きょう は/助詞/は 雨/名詞/あめ です/助動詞/です 。/記号/。"
     assert actual == expected
 
 
-def test__split() -> None:
-    line = "今日	名詞,副詞可能,*,*,*,*,今日,キョウ,キョー"
-    mecab = Mecab()
-    actual = mecab._split(line)
+def test__split(mecab, lines) -> None:
+    actual = mecab._split(lines[0])
     expected = ("今日", "名詞", "副詞可能", "きょう")
     assert actual == expected
