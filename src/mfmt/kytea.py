404d239d0546656f5865fdb300cbc5d385795ce9
jun-harashima <j.harashima@gmail.com>
2024-11-23 09:14:34 +0900

    modify k2m

--- src/mfmt/kytea.py
+++ src/mfmt/kytea.py
@@ -1,13 +1,17 @@
+from jaconv import hira2kata
+
+
 class Kytea:
     def __init__(self) -> None:
         pass
 
     def to_mecab(self, input_lines: list[str]) -> list[str]:
         output_lines = []
-        for input_line in input_lines:
-            for word in input_line.split():
+        for line in input_lines:
+            for word in line.split():
                 midashi, hinshi, yomi = word.split("/")
-                output_line = f"{midashi}\t{hinshi},{yomi}\n"
-                output_lines.append(output_line)
+                yomi = hira2kata(yomi)
+                line = f"{midashi}\t{hinshi},*,*,*,*,*,*,{yomi},*\n"
+                output_lines.append(line)
             output_lines.append("EOS\n")
         return output_lines
