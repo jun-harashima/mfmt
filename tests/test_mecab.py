735c92fe685388294d568a5601dae9b61ab8e43e
jun-harashima <j.harashima@gmail.com>
2024-11-24 09:18:36 +0900

    refactor tests

--- tests/test_mecab.py
+++ tests/test_mecab.py
@@ -14,32 +14,36 @@ def lines() -> list[str]:
     lines = [
         "今日	名詞,副詞可能,*,*,*,*,今日,キョウ,キョー",
         "は	助詞,係助詞,*,*,*,*,は,ハ,ワ",
-        "雨	名詞,一般,*,*,*,*,雨,アメ,アメ",
+        "よい	形容詞,自立,*,*,形容詞・アウオ段,基本形,よい,ヨイ,ヨイ",
+        "天気	名詞,一般,*,*,*,*,天気,テンキ,テンキ",
         "です	助動詞,*,*,*,特殊・デス,基本形,です,デス,デス",
         "。	記号,句点,*,*,*,*,。,。,。",
     ]
     return lines
 
 
-def test__to_juman(mecab, lines) -> None:
+def test__to_juman(mecab: Mecab, lines: list[str]) -> None:
     actual = mecab._to_juman(lines)
     expected = [
         "今日 きょう * 名詞 * 副詞可能 * * * * * *",
         "は は * 助詞 * 係助詞 * * * * * *",
-        "雨 あめ * 名詞 * 一般 * * * * * *",
+        "よい よい * 形容詞 * 自立 * * * * * *",
+        "天気 てんき * 名詞 * 一般 * * * * * *",
         "です です * 助動詞 * * * * * * * *",
         "。 。 * 記号 * 句点 * * * * * *",
     ]
     assert actual == expected
 
 
-def test__to_kytea(mecab, lines) -> None:
+def test__to_kytea(mecab: Mecab, lines: list[str]) -> None:
     actual = mecab._to_kytea(lines)
-    expected = ["今日/名詞/きょう は/助詞/は 雨/名詞/あめ です/助動詞/です 。/記号/。"]
+    expected = [
+        "今日/名詞/きょう は/助詞/は よい/形容詞/よい 天気/名詞/てんき です/助動詞/です 。/記号/。",  # noqa: E501
+    ]
     assert actual == expected
 
 
-def test__split(mecab, lines) -> None:
+def test__split(mecab: Mecab, lines: list[str]) -> None:
     actual = mecab._split(lines[0])
     expected = ("今日", "名詞", "副詞可能", "きょう")
     assert actual == expected
